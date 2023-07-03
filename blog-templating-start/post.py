025f648a4d13949352aeff7cdddfbc8eb8194d9f
Zachary Tokar <zack.tokar@gmail.com>
2023-07-03 14:27:20 -0400

    Finished added Flask links

--- blog-templating-start/post.py
+++ blog-templating-start/post.py
@@ -1,2 +1,8 @@
 class Post:
-    pass
+    def __init__(self,id,title,subtitle,body): 
+        self.id=id
+        self.title=title
+        self.subtitle=subtitle
+        self.body=body
+
+
--- solutions folder/main.py
+++ solutions folder/main.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template
-import requests
 from post import Post
+import requests
 
-
-
-posts=requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-
-all_posts=[]
+posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
+post_objects = []
 for post in posts:
-    post_obj=Post(post["id"], post["title"], post["subtitle"], post["body"])
-    all_posts.append(post_obj)
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return render_template("index.html",posts=all_posts)
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
-    for blog_post in all_posts:
+    for blog_post in post_objects:
         if blog_post.id == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+
